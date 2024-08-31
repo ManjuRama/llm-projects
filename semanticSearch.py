eb30b83878128d3b1b313ea713258e499bfb481e
Manju Ramasubbiah <106892067+ManjuRama@users.noreply.github.com>
2024-08-31 18:09:17 -0400

    Update semanticSearch.py

--- semanticSearch.py
+++ semanticSearch.py
@@ -146,11 +146,6 @@ def get_corpus (corpus_loc = CORPUS):
     separators=[
         "\n\n",
         "\n",
-        " ",
-        ".",
-        ",",
-        "\u200b",  # Zero-width space
-        "\uff0c",  # Fullwidth comma
         "\u3001",  # Ideographic comma
         "\uff0e",  # Fullwidth full stop
         "\u3002",  # Ideographic full stop
